1b3b09eb3dbb01ffba350067b195ea6d650aa140
guillaume14h28 <guillaume.schneyder@14h28.com>
2019-02-05 18:24:57 +0100

    gros css version pc

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+// Déclaration des paramètres de connexion
+$host = 'localhost';
+$user = 'root';
+$bdd = 'rive_gauche';
+$passwd = '';
+/*
+mysql_connect($host,$user,$passwd) or die("erreur de connexion au serveur $host");
+mysql_select_db($bdd) or die("erreur de connexion a la base de donnees");*/
+
+$conn = new mysqli($host, $user, $passwd, $bdd);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (!$conn->set_charset("utf8")) {
+    //printf("Erreur lors du chargement du jeu de caractères utf8 : %s\n", $conn->error);
+    exit();
+} else {
+    //printf("Jeu de caractères courant : %s\n", $conn->character_set_name());
+}
+
+?>
